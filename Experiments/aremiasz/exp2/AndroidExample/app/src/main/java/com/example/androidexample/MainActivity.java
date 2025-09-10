4907eacca07680af360d8e8eec60448052766355
Alex Remiasz <aremiasz@iastate.edu>
2025-09-10 14:10:57 -0500

    Push experiment 2

--- Experiments/aremiasz/exp2/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/aremiasz/exp2/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.androidexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import androidx.appcompat.app.AppCompatActivity;
 
 /*
 
@@ -43,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;     // define message textview variable
     private Button counterButton;     // define counter button variable
+    private Button userButton;        // define user button variable
+    private Button mapButton;         // define map button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,24 +53,60 @@ public class MainActivity extends AppCompatActivity {
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         counterButton = findViewById(R.id.main_counter_btn);// link to counter button in the Main activity XML
+        userButton = findViewById(R.id.main_user_btn);      // link to user button in the Main activity XML
+        mapButton = findViewById(R.id.main_map_btn);        // link to map button in the Main activity XML
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
-        if(extras == null) {
-            messageText.setText("Intent Example");
+        /* check which activity passed the intent */
+        if (extras != null) {
+            if (extras.containsKey("NUM")) { // if the key "NUM" exists, it came from CounterActivity
+                String num = extras.getString("NUM");
+                messageText.setText("Counter Value: " + num); // set message text to the counter value
+            } else if (extras.containsKey("USER")) { // if the key "USER" exists, it came from UserActivity
+                User user = (User) extras.getSerializable("USER");
+                messageText.setText("Name & Age: " + user.getName() + ", Age: " + user.getAge()); // set message text to the user info
+            }
         } else {
-            String number = extras.getString("NUM");  // this will come from LoginActivity
-            messageText.setText("The number was " + number);
+            messageText.setText("Intent Experiment");
         }
 
         /* click listener on counter button pressed */
         counterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 /* when counter button is pressed, use intent to switch to Counter Activity */
                 Intent intent = new Intent(MainActivity.this, CounterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        /* click listener on user button pressed */
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(intent);
+            }
+        });
+        /* click listener on map button pressed */
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+        /* click listener on title text pressed */
+        messageText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Hello!");
+                builder.setMessage("There is nothing to see here");
+                builder.setPositiveButton("OK!", null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
     }
