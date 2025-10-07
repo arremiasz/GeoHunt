6fca0447f875ac9babe1c609b8dc19f8cd1b40ec
evjulson <evjulson@iastate.edu>
2025-10-06 22:41:26 -0500

    moved LogInController to Controllers package

--- Backend/geolocation/Backend/src/main/java/com/geohunt/backend/Controllers/LogInController.java
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/Controllers/LogInController.java
@@ -1,5 +1,7 @@
-package com.geohunt.backend.database;
+package com.geohunt.backend.Controllers;
 
+import com.geohunt.backend.database.Account;
+import com.geohunt.backend.database.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
