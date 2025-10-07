dc0d6f68f84ae6dd0b2ea1f1c9101d67b2b8f42f
evjulson <evjulson@iastate.edu>
2025-10-06 21:54:47 -0500

    commented out session code to remove interference

--- Backend/geolocation/Backend/src/main/java/com/geohunt/backend/database/Account.java
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/database/Account.java
@@ -1,6 +1,9 @@
 package com.geohunt.backend.database;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
