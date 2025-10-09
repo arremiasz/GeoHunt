605f8d34433eccabcfe8c00d9049a0912ac28755
Alex Remiasz <aremiasz@iastate.edu>
2025-10-09 13:22:54 -0500

    Edit ApiConstants.java

--- Frontend/geohunt/app/src/main/java/com/jubair5/geohunt/network/ApiConstants.java
+++ Frontend/geohunt/app/src/main/java/com/jubair5/geohunt/network/ApiConstants.java
@@ -9,7 +9,7 @@ public final class ApiConstants {
     private ApiConstants() {}
 
     // Base URL for all API calls
-    public static final String BASE_URL = "http://coms-3090-030.class.las.iastate.edu:3306";
+    public static final String BASE_URL = "http://coms-3090-030.class.las.iastate.edu:8080";
 //    public static final String BASE_URL = "https://6bfe1ae1-ae39-462f-910e-7d53b5da9867.mock.pstmn.io";
 
     // Account Endpoints
