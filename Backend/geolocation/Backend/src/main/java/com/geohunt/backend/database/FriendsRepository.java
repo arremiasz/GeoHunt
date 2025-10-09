9660f4a99eda65011bbad12da704256df7c29f03
ArjavTripathi <arjavtri@iastate.edu>
2025-10-09 15:59:37 -0500

    KEEP

--- Backend/geolocation/Backend/src/main/java/com/geohunt/backend/database/FriendsRepository.java
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/database/FriendsRepository.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface FriendsRepository extends JpaRepository<Friends, FriendKey> {
     List<Friends> findByPrimary(Account primary);
+
+    List<Friends> findByTargetAndIsAcceptedTrue(Account target);
+
+    boolean existsByPrimaryAndTarget(Account primary, Account target);
 }
