ae250e0ae38df382f0e42ae3a62ee8d303bac67b
Alex Remiasz <aremiasz@iastate.edu>
2025-10-29 12:28:01 -0500

    Implemented home screen with map and play button

---
+++ Frontend/geohunt/app/src/main/java/com/jubair5/geohunt/game/GameActivity.java
@@ -0,0 +1,16 @@
+package com.jubair5.geohunt.game;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jubair5.geohunt.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
