d4d752222eee549aed614f762d0fabccba0a6633
evjulson <evjulson@iastate.edu>
2025-11-02 21:13:44 -0600

    Basic Multiplayer Websocket Functionality

--- Backend/geolocation/Backend/src/main/java/com/geohunt/backend/multiplayer/Lobby.java
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/multiplayer/Lobby.java
@@ -1,4 +1,33 @@
 package com.geohunt.backend.multiplayer;
 
+
+import com.geohunt.backend.database.Account;
+import com.geohunt.backend.database.AccountService;
+import jakarta.websocket.server.ServerEndpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.xml.stream.Location;
+import java.util.List;
+
 public class Lobby {
+
+    static final int MAX_PLAYERS = 8;
+
+    // Services
+    @Autowired
+    AccountService accountService;
+
+    // Connected Players
+    List<Account> connectedPlayers;
+    Account lobbyLeader;
+
+    // Settings
+    double radius;
+    Location radiusCenter;
+    boolean powerUpsEnabled;
+
+    public Lobby(){
+
+    }
 }
---
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/multiplayer/MultiplayerController.java
@@ -0,0 +1,104 @@
+package com.geohunt.backend.multiplayer;
+
+
+import com.geohunt.backend.database.AccountService;
+import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@ServerEndpoint("/multiplayer/{username}")
+public class MultiplayerController {
+
+    public static AccountService accountService;
+
+    @Autowired
+    public void setAccountService(AccountService service){ accountService = service; }
+
+    private static Map<Session, String> sessionUsernameMap = new Hashtable<>();
+    private static Map<String, Session> usernameSessionMap = new Hashtable<>();
+
+    private static Map<String, Lobby> usernameLobbyMap = new HashMap<>();
+
+    private final Logger logger = LoggerFactory.getLogger(MultiplayerController.class);
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) throws IOException{
+        // User connects to the server and is ready to join or create a lobby
+        logger.info("Entered into Open");
+
+        // Check for valid username
+        try {
+            accountService.getAccountByUsername(username);
+        }
+        catch (IllegalArgumentException e){
+            session.close(new CloseReason(CloseReason.CloseCodes.VIOLATED_POLICY, "Invalid username"));
+            // User has tried to connect with a username that does not exist, and should fail to connect.
+        }
+
+        // Store connecting user information
+        sessionUsernameMap.put(session, username);
+        usernameSessionMap.put(username, session);
+
+        // Send confirmation message
+        sendStringToSingleUser(username, "Connection Success");
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message){
+        // If the user is not in a lobby, they can create or join one.
+        // If a user is in a lobby, the request is handled by the lobby.
+        logger.info("Entered into Message: Got Message:" + message);
+
+        sendStringToSession(session, "Received: " + message);
+    }
+
+    @OnClose
+    public void onClose(Session session){
+        // Remove player from lobby.
+        logger.info("Entered into Close");
+
+        String username = sessionUsernameMap.get(session);
+        sessionUsernameMap.remove(session);
+        usernameSessionMap.remove(username);
+        usernameLobbyMap.remove(username);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        // Do error handling here
+        logger.info("Entered into Error");
+        throwable.printStackTrace();
+    }
+
+    public void sendStringToUserList(List<String> usernames, String message){
+        for(String username : usernames) {
+            sendStringToSingleUser(username, message);
+        }
+    }
+
+    public void sendStringToSingleUser(String username, String message){
+        Session session = usernameSessionMap.get(username);
+        sendStringToSession(session, message);
+    }
+
+    public void sendStringToSession(Session session, String message){
+        try {
+            session.getBasicRemote().sendText(message);
+        }
+        catch (IOException e) {
+            logger.info("Exception: " + e.getMessage().toString());
+            e.printStackTrace();
+        }
+    }
+}
