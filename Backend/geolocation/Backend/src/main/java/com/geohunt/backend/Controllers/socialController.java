c7714672066603485cf94eff8a4962980c4f0f86
Speedywtff <speedyjake08@gmail.com>
2025-11-05 19:46:23 -0600

    Latest

--- Backend/geolocation/Backend/src/main/java/com/geohunt/backend/Controllers/socialController.java
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/Controllers/socialController.java
@@ -1,11 +1,13 @@
 package com.geohunt.backend.Controllers;
 
+import com.geohunt.backend.database.Account;
 import com.geohunt.backend.Services.AccountService;
 import com.geohunt.backend.database.FriendsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class socialController {
@@ -15,9 +17,29 @@ public class socialController {
     @Autowired
     private FriendsService friendsService;
 
-    @PutMapping("/friends/add")
+    @GetMapping("/friends")
+    public ResponseEntity<ArrayList<Account>> getFriends(@RequestParam(required = true) long id){
+        return friendsService.getFriends(id);
+    }
+
+    @PostMapping("/friends/add")
     public ResponseEntity<String> addFriend(@RequestParam long primaryId, @RequestParam long targetId) {
         return friendsService.addFriend(primaryId, targetId);
+    }
+
+    @DeleteMapping("/friends/remove")
+    public ResponseEntity<String> removeFriend(@RequestParam long primaryId, @RequestParam long targetId) {
+        return friendsService.removeFriend(primaryId, targetId);
+    }
+
+    @DeleteMapping("/friends/reject")
+    public ResponseEntity<String> rejectFriend(@RequestParam long primaryId, @RequestParam long targetId) {
+        return friendsService.rejectFriend(primaryId, targetId);
+    }
+
+    @PutMapping("/friends/accept")
+    public ResponseEntity<String> acceptFriend(@RequestParam long primaryId, @RequestParam long targetId) {
+        return friendsService.acceptFriend(primaryId, targetId);
     }
 
 }
--- Backend/geolocation/Backend/src/main/java/com/geohunt/backend/database/FriendsService.java
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/database/FriendsService.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,6 +18,24 @@ public class FriendsService {
     @Autowired
     private AccountRepository accountRepository;
 
+    public ArrayList<Optional<Account>> doesAccountsExist(long primaryId, long targetId) {
+        Optional<Account> account = accountRepository.findById(primaryId);
+        Optional<Account> target = accountRepository.findById(targetId);
+        ArrayList<Optional<Account>> returnable = new ArrayList<>();
+
+        if (account.isPresent() && target.isPresent()) {
+
+            returnable.add(account);
+            returnable.add(target);
+            return returnable;
+        } else if(account.isPresent() && !target.isPresent()) {
+            returnable.add(account);
+        } else if(target.isPresent() && !account.isPresent()) {
+            returnable.add(target);
+        }
+        return new ArrayList<>();
+    }
+
     public ResponseEntity<String> addFriend(Long senderId, Long targetId){
         if (senderId.equals(targetId)) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("You cannot friend  yourself.");
@@ -26,22 +46,17 @@ public class FriendsService {
         key.setTargetId(targetId);
 
         if (friendsRepository.existsById(key)) {
-            throw new IllegalArgumentException("Friend request already exists.");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Friend already exists.");
         }
         Optional<Account> acc = accountRepository.findById(senderId);
         Optional<Account> target = accountRepository.findById(targetId);
 
         if(acc.isEmpty()) {
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sender not found.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sender not found.");
         } else if(target.isEmpty()) {
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body("Target not found.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Target not found.");
         }
 
-        boolean alreadyExists = friendsRepository.existsByPrimaryAndTarget(acc.get(), target.get());
-
-        if(alreadyExists) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Friend already exists.");
-        }
 
         Friends friendship = new Friends();
         friendship.setId(key);
@@ -51,5 +66,86 @@ public class FriendsService {
 
         friendsRepository.save(friendship);
         return ResponseEntity.status(HttpStatus.CREATED).body("Friend added.");
+    }
+
+    public ResponseEntity<String> removeFriend(Long accountId, Long targetId) {
+        ArrayList<Optional<Account>> Accounts = doesAccountsExist(accountId, targetId);
+        if (Accounts.size() == 1) {
+            Optional<Account> account = Accounts.get(0);
+            if(account.get().getId() == targetId) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sender not found.");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Target not found.");
+            }
+        }
+        Account acc = Accounts.get(0).get();
+        Account target = Accounts.get(1).get();
+
+        Optional<Friends> friendship = friendsRepository.findByPrimaryAndTarget(acc, target);
+        if(friendship.isEmpty()) {return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Both accounts are not friends.");}
+
+
+        friendsRepository.delete(friendship.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Friend removed.");
+    }
+
+    public ResponseEntity<String> acceptFriend(Long primaryId, Long targetId) {
+        ArrayList<Optional<Account>> Accounts = doesAccountsExist(primaryId, targetId);
+        if (Accounts.size() == 1) {
+            Optional<Account> account = Accounts.get(0);
+            if(account.get().getId() == targetId) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sender not found.");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Target not found.");
+            }
+        }
+        Account acc;
+        Account target;
+        try{
+            acc = Accounts.get(0).get();
+            target = Accounts.get(1).get();
+        } catch (IndexOutOfBoundsException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cannot find account of main or target user.");
+        }
+
+
+        Optional<Friends> friendship = friendsRepository.findByPrimaryAndTarget(acc, target);
+        if(friendship.isEmpty()) {return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Friend Request not sent.");}
+        if(friendship.get().isAccepted()) {return ResponseEntity.status(HttpStatus.CONFLICT).body("Friend already accepted.");}
+        friendship.get().setAccepted(true);
+        friendsRepository.save(friendship.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Friend accepted.");
+    }
+
+    public ResponseEntity<ArrayList<Account>> getFriends(long id){
+        Optional<Account> account = accountRepository.findById(id);
+        if(account.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ArrayList<>());
+        }
+        List<Friends> friends = friendsRepository.findByPrimaryAndIsAcceptedTrue(account.get());
+        ArrayList<Account> accounts = new ArrayList<>();
+        for(Friends friend : friends) {
+            accounts.add(friend.getTarget());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(accounts);
+    }
+
+    public ResponseEntity<String> rejectFriend(Long primaryId, Long targetId) {
+        ArrayList<Optional<Account>> Accounts = doesAccountsExist(primaryId, targetId);
+        if (Accounts.size() == 1) {
+            Optional<Account> account = Accounts.get(0);
+            if(account.get().getId() == targetId) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sender not found.");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Target not found.");
+            }
+        }
+        Account acc = Accounts.get(0).get();
+        Account target = Accounts.get(1).get();
+
+        Optional<Friends> friendship = friendsRepository.findByPrimaryAndTarget(acc, target);
+        if(friendship.isEmpty()) {return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Friend Request does not exist.");}
+        friendsRepository.delete(friendship.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Friend removed.");
     }
 }
