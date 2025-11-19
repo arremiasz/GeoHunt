800ebcca2507bfbe086ccd78866d383ed01022b0
Speedywtff <speedyjake08@gmail.com>
2025-11-19 17:29:02 -0600

    demo change

--- Backend/geolocation/Backend/src/main/java/com/geohunt/backend/Services/AccountService.java
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/Services/AccountService.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
-
     @Autowired
     private FriendsService friendsService;
 
