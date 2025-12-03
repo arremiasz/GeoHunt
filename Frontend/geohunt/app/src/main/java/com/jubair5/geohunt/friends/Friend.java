a28fe681c3ca08c13bfe6814b74a90ac751dc008
Nathan Imig <nmimig@iastate.edu>
2025-12-03 06:18:51 -0600

    Comments Everything

--- Frontend/geohunt/app/src/main/java/com/jubair5/geohunt/friends/Friend.java
+++ Frontend/geohunt/app/src/main/java/com/jubair5/geohunt/friends/Friend.java
@@ -15,6 +15,10 @@ public class Friend {
     private String photoUrl;
 
 
+    /**
+     * Creates a friend and relationship based on json
+     * @param json
+     */
     public Friend(JSONObject json) {
         id = json.optInt("id");
         username = json.optString("username");
@@ -22,17 +26,40 @@ public class Friend {
         state = json.optInt("state");
     }
 
+    /**
+     * Gets the decoded image data as a byte array.
+     * @return The photo data, ready to be loaded by Glide.
+     */
     private String getPhotoUrl(){return photoUrl;}
+
+    /**
+     * returns the id of the account
+     * @return id of the account
+     */
     public int getId() {
         return id;
     }
+
+    /**
+     * returns the username of the account
+     * @return username of the account
+     */
     public String getUsername() {
         return username;
     }
+
+    /**
+     * returns the state of relationship
+     * @return state of relationship
+     */
     public int getState() {
         return state;
     }
 
+    /**
+     * sets the state of the relationship
+     * @param state
+     */
     public void setState(int state) {
         this.state = state;
     }
