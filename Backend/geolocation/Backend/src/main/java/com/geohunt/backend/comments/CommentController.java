9b8b77b869751c76aff11461452c9c3f79b7d08c
evjulson <evjulson@iastate.edu>
2025-12-03 16:34:23 -0600

    Comments Get, Delete, List Methods

--- Backend/geolocation/Backend/src/main/java/com/geohunt/backend/comments/CommentController.java
+++ Backend/geolocation/Backend/src/main/java/com/geohunt/backend/comments/CommentController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CommentController {
@@ -27,11 +24,15 @@ public class CommentController {
     ChallengesRepository challengesRepository;
 
     // Post
-    @PostMapping("challenge/{id}/comments")
-    public ResponseEntity<Comment> postComment(@PathVariable long challengeId, @RequestParam long accountId, @RequestBody String comment){
+    @PostMapping("challenges/{cid}/comments")
+    public ResponseEntity<Comment> postComment(@PathVariable long cid, @RequestParam long uid, @RequestBody String comment){
         try{
-            Account author = accountService.getAccountById(accountId);
-            Challenges challenges = challengesRepository.findById(challengeId).get();
+            if(challengesRepository.findById(cid).isEmpty()){
+                return ResponseEntity.badRequest().build();
+            }
+
+            Account author = accountService.getAccountById(uid);
+            Challenges challenges = challengesRepository.findById(cid).get();
 
             Comment newComment = new Comment(author, challenges, comment);
             commentRepository.save(newComment);
@@ -43,10 +44,47 @@ public class CommentController {
     }
 
     // Get
+    @GetMapping("comments/{id}")
+    public ResponseEntity<Comment> getComment(@PathVariable long id){
+        if(commentRepository.findById(id).isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(commentRepository.findById(id).get());
+    }
 
     // Put
 
     // Delete
+    @DeleteMapping("/comments/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable long id){
+        if(commentRepository.findById(id).isEmpty()){
+            return ResponseEntity.badRequest().build();
+        }
+        Comment comment = commentRepository.findById(id).get();
+        commentRepository.delete(comment);
+        return ResponseEntity.ok("Removed comment");
+    }
 
     // List
+    @GetMapping("/challenges/{cid}/comments")
+    public ResponseEntity<Comment[]> getCommentsByChallenge(@PathVariable long cid){
+        if(challengesRepository.findById(cid).isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Challenges challenges = challengesRepository.findById(cid).get();
+        Comment[] commentArr = challenges.getComments().toArray(new Comment[0]);
+        return ResponseEntity.ok(commentArr);
+    }
+
+    @GetMapping("/account/{uid}/comments")
+    public ResponseEntity<Comment[]> getCommentsByUser(@PathVariable long uid){
+        try{
+            Account account = accountService.getAccountById(uid);
+            Comment[] commentArr = account.getComments().toArray(new Comment[0]);
+            return ResponseEntity.ok(commentArr);
+        }
+        catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
