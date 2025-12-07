bc22a72321ed335e8a1b2af75f5194804060b271
evjulson <evjulson@iastate.edu>
2025-12-07 16:49:33 -0600

    Mocks for Login Test

--- Backend/geolocation/Backend/src/test/java/com/geohunt/backend/EvanSystemTestLogin.java
+++ Backend/geolocation/Backend/src/test/java/com/geohunt/backend/EvanSystemTestLogin.java
@@ -1,12 +1,20 @@
 package com.geohunt.backend;
 
+import com.geohunt.backend.Controllers.LogInController;
+import com.geohunt.backend.Services.AccountService;
+import com.geohunt.backend.database.AccountRepository;
 import io.restassured.RestAssured;
+
 import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,6 +24,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class EvanSystemTestLogin {
 
     private AutoCloseable mocks;
+
+    @InjectMocks
+    LogInController logInController;
+
+    @Mock
+    AccountService mockAccountService;
 
     @LocalServerPort
     int port = 8080;
@@ -33,7 +47,6 @@ public class EvanSystemTestLogin {
 
     @Test
     public void testLogin(){
-
     }
 
     @AfterEach
